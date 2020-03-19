c3a5e2892e86bfd07b81033dc05cea7f6d8b57b1
sanywright13 <axil.wright@gmail.com>
2020-03-19 16:18:05 +0100

    add react requerement

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 import random
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='/home/sanaa/PycharmProjects/test-react-flask/static/dist', template_folder='/home/sanaa/PycharmProjects/test-react-flask/static')
+app = Flask(__name__, static_folder='../static/dist', template_folder='../static')
 
 @app.route('/')
 def index():
--- static/js/App.jsx
+++ static/js/App.jsx
@@ -5,23 +5,21 @@ import { PageHeader } from "react-bootstrap";
 require('/home/sanaa/PycharmProjects/test-react-flask/static/css/fullstack.css');
 var $ = require('jquery');
 
-import HeaderBackgroundImage from '/home/sanaa/PycharmProjects/test-react-flask/static/images/header.jpg';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
     }
-    addHeaderImg() {
-        let headerBg = new Image();
-        headerBg.src = HeaderBackgroundImage;
-    }
 
+componentDidMount(){
+  fetch('https://api.themoviedb.org/3/movie/419704?api_key=00dd795327e61a36353c4ebdcb82df44&language=en-US').
+    then(response => response.json() ).then(data => console.log(data.genres));
+
+}
     render () {
         return (
             <PageHeader>
                 <div className='header-contents'>
-                {this.addHeaderImg()}
-                <Hello name='Rimini' />
                 </div>
             </PageHeader>
         );
