07f1f2d38ee0de686fb06a43394aa7b2356aea39
sanywright13 <axil.wright@gmail.com>
2020-03-20 01:23:33 +0100

    build project flask + react

--- server/server.py
+++ server/server.py
@@ -1,14 +1,18 @@
 import random
 from flask import Flask, render_template
+from pymongo import MongoClient
+from marshmallow import Schema, fields
 
-app = Flask(__name__, static_folder='/home/sanaa/PycharmProjects/test-react-flask/static/dist', template_folder='/home/sanaa/PycharmProjects/test-react-flask/static')
+app = Flask(__name__, static_folder='/home/sanaa/PycharmProjects/test-react-flask/static/dist',
+            template_folder='/home/sanaa/PycharmProjects/test-react-flask/static')
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/hello') # take note of this decorator syntax, it's a common pattern
+@app.route('/hello')  # take note of this decorator syntax, it's a common pattern
 def hello():
     # It is good practice to only call a function in your route end-point,
     # rather than have actual implementation code here.
@@ -17,8 +21,23 @@ def hello():
 
 
 def get_hello():
-    greeting_list = 'hi'
-    return (greeting_list)
+    class MovieSchema(Schema):
+        author = fields.Str()
+        text = fields.Str()
+
+    client = MongoClient('localhost', 27017)
+    db = client.moviesdb
+    collection = db.populaires
+    movie = MovieSchema(author="Monty", text="monty@python.org")
+
+    posts = collection
+    post = posts.insert_one(movie)
+
+    print(collection.find_one({"author": "Monty"}))
+    col = collection.find_one({"author": "Monty"})
+    name = col['author']
+    print(name)
+    return (name)
 
 
 if __name__ == '__main__':
--- static/js/App.jsx
+++ static/js/App.jsx
@@ -8,9 +8,33 @@ var $ = require('jquery');
 export default class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state= {
+          genres:[],
+            name:'',
+            resume:''
+        }
         this.getPythonHello=this.getPythonHello.bind(this);
     }
+componentDidMount() {
+fetch('https://api.themoviedb.org/3/movie/419704?api_key=00dd795327e61a36353c4ebdcb82df44&language=en-US')
+  .then(response => response.json())
+  .then(data => {
+        let  genres_updated = data.genres.map((genres)=>{
+           return genres.name
 
+        })
+
+
+  this.setState(
+  {
+genres: genres_updated,
+      name:data.name,
+      resume:data.overview
+
+})
+}
+  );
+}
     getPythonHello() {
    $.get(window.location.href + 'hello', (data) => {
             console.log(data);
@@ -20,13 +44,18 @@ export default class App extends React.Component {
     }
     render () {
         return (
-            <PageHeader>
-          <div>     <button onClick={this.getPythonHello}>
-                    Clicker
 
-                </button>
+          <div>
+
+
+genres : {this.state.genres}
+
+     name:          {this.state.name}
+resume :              {this.state.resume}
+
+<button onClick={this.getPythonHello}>click</button>
               </div>
-            </PageHeader>
+
         );
     }
 }
