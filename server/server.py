363767697d8bea9376c5b8b066ea5e05bc688f94
sanywright13 <axil.wright@gmail.com>
2020-03-25 20:00:01 +0100

    design app2

--- server/server.py
+++ server/server.py
@@ -16,6 +16,8 @@ mongo = MongoClient('localhost', 27017)
 db = mongo['dbmovies']
 mongo = MongodbMovie()
 shemaMovie = MovieShema()
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,7 +25,6 @@ def index():
 
 @app.route('/addMovie', methods=["POST"])
 def addMovie():
-
     # i want this record to be an object
     # i want to get the movie object from react and insert it here
     # serialize the api object so we can add it to a data base
@@ -34,10 +35,10 @@ def addMovie():
     movie_objet = shemaMovie.dump(jsondata['item'])
     mongo.addmovie(movie_objet)
     # convert to an object movie
-    #movie_objet = shemaMovie.load(jsondata['item'])
+    # movie_objet = shemaMovie.load(jsondata['item'])
     # print(movie_objet.title)
     ## convert to a string json
-    #json_result = shemaMovie.dumps(movie_objet)
+    # json_result = shemaMovie.dumps(movie_objet)
     # print(json_result)
     # we want to return thr data base object
     return 'hi'
@@ -47,15 +48,8 @@ def addMovie():
 def get_all_movies():
     # get the list of the movies added in the data base
 
-    data=[shemaMovie.dump(movie) for movie in mongo.all_movies()]
+    data = [shemaMovie.dump(movie) for movie in mongo.all_movies()]
     return jsonify(data)
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
--- static/js/favoritefilms.js
+++ static/js/favoritefilms.js
@@ -1,26 +1,77 @@
 import React, {Component} from "react";
 import APIClient from "./apiClient";
-
-
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import {Grid} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Avatar from '@material-ui/core/Avatar';
 class  FavoriteFilms extends Component {
+
         constructor(props) {
                 super(props);
                 this.state={
                         faveMovies:[]
                 }
         }
+
 componentDidMount(){
    //get all the films in db
    let     apiClient=new APIClient();
 
-apiClient.getAllMovies().then(data=>console.log(data))
+apiClient.getAllMovies().then(data=> {
 
+            this.setState(prev => {
+                    return {faveMovies: data}
 
+            })
+    }
+
+)
 }
-        render() {
+     render() {
 
-                return (<div><h1>Vos Films Favoris</h1></div>);
-
-        }
+            return (
+   <div >
+           {this.state.faveMovies.map(row=>{
+                          return (
+      <Paper >
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase >
+              <img  alt="complex" src="/static/images/grid/complex.jpg" />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">
+                    {row.title}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Full resolution 1920x1080 • JPEG
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  ID: 1030114
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1">$19.00</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+                              )})}
+    </div>
+  );
+}
 }
 export default FavoriteFilms
